9519baa729d4e6921d666bd92f5899082e3c93da
jaymompean <jaymompean@gmail.com>
2018-12-20 15:05:56 -0800

    plugin update to add react dependencies

--- ss_templates/includes/-SMP-js-plugins_pagespeed.php
+++ ss_templates/includes/-SMP-js-plugins_pagespeed.php
@@ -17,6 +17,11 @@
 <script src="/AE/js/_pagespeed/hoverIntent.min.js" type="text/javascript"></script>
 <script src="/AE/js/_pagespeed/smp-nav-kyle.js" type="text/javascript"></script>
 
+<!-- Load React. -->
+<script src="https://unpkg.com/react@16/umd/react.production.min.js" crossorigin></script>
+<script src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js" crossorigin></script>
+
+<!-- Lazyload imgs -->
 <script src="https://cdn.jsdelivr.net/npm/vanilla-lazyload@10.19.0/dist/lazyload.min.js"></script>
 <!-- <script src="https://cdn.jsdelivr.net/npm/vanilla-lazyload@8.17.0/dist/lazyload.min.js"></script>
 
