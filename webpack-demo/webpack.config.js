de2fc40a3de14c0ac89714d43a1395eca8d17321
jaymompean <jaymompean@gmail.com>
2019-01-03 15:01:18 -0800

    added HtmlWebpackPlugin

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,33 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+},
+  plugins: {
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
-        use: [
-          'file-loader'
-        ]
-      }
-    ]
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
   }
 };
