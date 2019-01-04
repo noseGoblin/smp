095cc9ea9a83b1762513d414e96cf51a7f8a2c60
jaymompean <jaymompean@gmail.com>
2019-01-03 17:43:33 -0800

    fixed errors in webpack and started removing unecessary css from bootstrap.min.css

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,16 +1,19 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'devlopment',
   entry: {
     app: './src/index.js',
     print: './src/print.js'
 },
-  plugins: {
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Output Management'
     })
-  },
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
